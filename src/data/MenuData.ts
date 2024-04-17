cb01ff850c3b1e8ff611645fad3d2118e18f1e30
sepool7 <rajaee.sepideh@gmail.com>
2024-04-17 22:25:36 +0200

    link

--- src/data/MenuData.ts
+++ src/data/MenuData.ts
@@ -55,7 +55,7 @@ const menu_data: MenuItem[] = [
         id: 7,
         has_dropdown: false,
         title: "DeFi Wallet",
-        link: "https://vectoriumdefi.xyz/LaqiraFarmingEligibility",
+        link: "https://vectoriumdefi.xyz",
     },
 ];
 export default menu_data;
