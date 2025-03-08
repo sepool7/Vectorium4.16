d49c5523afbf60e54d99447150d639659fec409e
Vectorium-project <48999278+Vectorium-project@users.noreply.github.com>
2025-03-08 17:38:50 +0530

    Update SEO.tsx

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,21 +1,21 @@
-import { Helmet } from "react-helmet-async";
+// import { Helmet } from "react-helmet-async";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const SEO = ({ pageTitle }:any) => {
-  return (
-    <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>{pageTitle} - ICO & Crypto Landing Page React Next js Template</title>
-        <meta name="robots" content="noindex, follow" />
-        <meta name="description" content="Zibber - Consulting React Template" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-      </Helmet>
-    </>
-  );
-};
+// // eslint-disable-next-line @typescript-eslint/no-explicit-any
+// const SEO = ({ pageTitle }:any) => {
+//   return (
+//     <>
+//       <Helmet>
+//         <meta charSet="utf-8" />
+//         <title>{pageTitle} - ICO & Crypto Landing Page React Next js Template</title>
+//         <meta name="robots" content="noindex, follow" />
+//         <meta name="description" content="Zibber - Consulting React Template" />
+//         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+//       </Helmet>
+//     </>
+//   );
+// };
 
-export default SEO;
+// export default SEO;
 
 
 // import { Helmet } from "react-helmet-async";
@@ -47,4 +47,25 @@ export default SEO;
 // };
 
 // export default SEO;
+
+import { Helmet } from "react-helmet-async";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const SEO = ({ pageTitle }:any) => {
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{pageTitle} - Revolutionary Blockchain Carbon Credit Platform React Next js Template</title>
+        <meta name="robots" content="noindex, follow" />
+        <meta name="description" content="Transform your carbon footprint with Vectorium's innovative blockchain-based carbon credit platform. Leading the eco-friendly revolution in digital carbon credit trading and verification " />
+        <meta property="og:image" content="https://freecoins24.io/wp-content/uploads/2024/07/dtxm4BGB_400x400.jpg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      </Helmet>
+    </>
+  );
+};
+
+export default SEO;
+
 
